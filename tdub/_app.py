ebb8ca4fd4b927e0526968d5912f272a9816a82e
Doug Davis <ddavis@ddavis.io>
2019-09-26 12:38:58 -0400

    move logging initialization to cli (out of __init__.py)

--- tdub/_app.py
+++ tdub/_app.py
@@ -119,7 +119,14 @@ def cli():
         parser.print_help()
         return 0
 
+    # fmt: off
+    logging.basicConfig(level=logging.INFO, format="{:20}  %(levelname)s  %(message)s".format("[%(name)s]"))
+    logging.addLevelName(logging.WARNING, "\033[1;31m{:8}\033[1;0m".format(logging.getLevelName(logging.WARNING)))
+    logging.addLevelName(logging.ERROR, "\033[1;35m{:8}\033[1;0m".format(logging.getLevelName(logging.ERROR)))
+    logging.addLevelName(logging.INFO, "\033[1;32m{:8}\033[1;0m".format(logging.getLevelName(logging.INFO)))
+    logging.addLevelName(logging.DEBUG, "\033[1;34m{:8}\033[1;0m".format(logging.getLevelName(logging.DEBUG)))
     log = logging.getLogger("tdub.cli")
+    # fmt: on
 
     if args.action == "regions2parquet":
         return _parquet_regions(args, log)
