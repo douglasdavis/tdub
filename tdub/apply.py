c72758af44fce036752cd66fcfcb6825d2935250
Doug Davis <ddavis@ddavis.io>
2019-09-27 17:26:18 -0400

    move minor bkg applier out of class (standalone function)

--- tdub/apply.py
+++ tdub/apply.py
@@ -95,80 +95,85 @@ class FoldedResult:
     def folder(self) -> KFold:
         return self._folder
 
-    def apply_to_minor(
-        self,
-        infile: Union[str, os.PathLike],
-        tree: str = "WtLoop_nominal",
-        weight_name: str = "weight_nominal",
-        outfile: Optional[str] = None,
-        use_root_pandas = False,
-    ) -> None:
-        """apply the folded result to a single minor background file
+def to_minorbkg(
+    folded_result: FoldedResult,
+    files: Union[str, List[str]],
+    tree: str = "WtLoop_nominal",
+    weight_name: str = "weight_nominal",
+    outfile: Optional[str] = None,
+    use_root_pandas = False,
+) -> None:
+    """apply the folded result to a single minor background file
 
-        Parameters
-        ----------
-        infile : str or os.PathLike
-           the input file to open and apply
-        tree : str
-           the name of the tree to extract data from
-        weight_name : str
-           name of the weight branch
-        outfile : str (optional)
-           output file name
-        use_root_pandas : bool
-           use root_pandas instead of uproot for writing the file (uproot's
-           TTree writing is pretty new and has some kinks to iron out)
+    Parameters
+    ----------
+    folded_result : FoldedResult
+       folded training class holding models to apply
+    files : str or os.PathLike
+       the input file to open and apply to
+    tree : str
+       the name of the tree to extract data from
+    weight_name : str
+       name of the weight branch
+    outfile : str (optional)
+       output file name
+    use_root_pandas : bool
+       use root_pandas instead of uproot for writing the file (uproot's
+       TTree writing is pretty new and has some kinks to iron out)
 
-        """
-        dfim = specific_dataframe(
-            infile, self.region, tree=tree, weight_name=weight_name, to_ram=True
-        )
-        dfim._df = dfim.df[self.features]
+    """
+    dfim = specific_dataframe(
+        files, folded_result.region, tree=tree, weight_name=weight_name, to_ram=True
+    )
+    dfim._df = dfim.df[folded_result.features]
 
-        X = dfim.df.to_numpy()
-        y0 = self.model0.predict_proba(X)[:, 1]
-        y1 = self.model1.predict_proba(X)[:, 1]
-        y2 = self.model2.predict_proba(X)[:, 1]
-        y = np.mean([y0, y1, y2], axis=0)
+    X = dfim.df.to_numpy()
+    y0 = folded_result.model0.predict_proba(X)[:, 1]
+    y1 = folded_result.model1.predict_proba(X)[:, 1]
+    y2 = folded_result.model2.predict_proba(X)[:, 1]
+    y = np.mean([y0, y1, y2], axis=0)
 
-        if self.region == Region.r1j1b:
-            reg = "reg1j1b"
-        elif self.region == Region.r2j1b:
-            reg = "reg2j1b"
-        elif self.region == Region.r2j2b:
-            reg = "reg2j2b"
+    if folded_result.region == Region.r1j1b:
+        reg = "reg1j1b"
+    elif folded_result.region == Region.r2j1b:
+        reg = "reg2j1b"
+    elif folded_result.region == Region.r2j2b:
+        reg = "reg2j2b"
 
-        reg_branch = np.array([1 for _ in range(dfim.df.shape[0])], dtype=np.int32)
-        OS_branch = reg_branch
-        weight_branch = dfim.weights.to_numpy()
+    reg_branch = np.array([1 for _ in range(dfim.df.shape[0])], dtype=np.int32)
+    OS_branch = reg_branch
+    weight_branch = dfim.weights.to_numpy()
 
-        if outfile is None:
-            outfile = "{}_applied.root".format(str(infile))
+    if outfile is None:
+        outfile = "_applied.root"
 
-        if use_root_pandas:
-            import root_pandas
-            dfim._df[weight_name] = weight_branch
-            dfim._df[reg] = reg_branch
-            dfim._df["OS"] = OS_branch
-            dfim._df[f"bdt_reponse_{reg}"] = y
-            root_pandas.to_root(dfim._df, outfile, key=tree)
+    if use_root_pandas:
+        import root_pandas
+        dfim._df[weight_name] = weight_branch
+        dfim._df[reg] = reg_branch
+        dfim._df["OS"] = OS_branch
+        dfim._df[f"bdt_reponse_{reg}"] = y
+        root_pandas.to_root(dfim._df, outfile, key=tree)
 
-        else:
-            out_dict = {}
-            out_dict[reg] = reg_branch
-            out_dict["OS"] = OS_branch
-            out_dict[weight_name] = weight_branch
-            out_dict[f"bdt_response_{reg}"] = y
-            for c in list(dfim.df.columns):
-                out_dict[c] = dfim.df[c].to_numpy()
+    else:
+        out_dict = {}
+        out_dict[reg] = reg_branch
+        out_dict["OS"] = OS_branch
+        out_dict[weight_name] = weight_branch
+        out_dict[f"bdt_response_{reg}"] = y
+        for c in list(dfim.df.columns):
+            out_dict[c] = dfim.df[c].to_numpy()
 
-            out_types = {}
-            for c, a in out_dict.items():
-                if a.dtype == np.uint32:
-                    out_dict[c] = out_dict[c].astype(np.int32)
-                out_types[c] = uproot.newbranch(out_dict[c].dtype)
-                log.info(f"Saving branch {c} with dtype {out_types[c]}")
+        out_types = {}
+        for c, a in out_dict.items():
+            if a.dtype == np.uint32:
+                out_dict[c] = out_dict[c].astype(np.int32)
+            out_types[c] = uproot.newbranch(out_dict[c].dtype)
+            log.info(f"Saving branch {c} with dtype {out_dict[c].dtype}")
 
-            with uproot.recreate(outfile) as f:
-                f[tree] = uproot.newtree(out_types, flushsize="10 MB")
-                f[tree].extend(out_dict, flush=False)
+        with uproot.recreate(outfile) as f:
+            f[tree] = uproot.newtree(out_types, flushsize="10 MB")
+            f[tree].extend(out_dict, flush=False)
+
+
+FoldedResult.to_minorbkg = to_minorbkg
