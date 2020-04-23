da89359b4dc6214e5f9d11f67371c9979b6e4d5e
Doug Davis <ddavis@ddavis.io>
2020-04-22 20:41:39 -0400

    logging formatting

--- tdub/__init__.py
+++ tdub/__init__.py
@@ -10,10 +10,10 @@ def setup_logging():
 
     # fmt: off
     logging.basicConfig(
-        level=logging.INFO, format="{:22}  %(levelname)8s  %(message)s".format("[%(name)s : %(funcName)s]")
+        level=logging.INFO, format="%(name)-15s %(funcName)-22s %(levelname)-8s    %(message)s"
     )
-    logging.addLevelName(logging.WARNING, "\033[1;31m{:8}\033[1;0m".format(logging.getLevelName(logging.WARNING)))
-    logging.addLevelName(logging.ERROR, "\033[1;35m{:8}\033[1;0m".format(logging.getLevelName(logging.ERROR)))
-    logging.addLevelName(logging.INFO, "\033[1;32m{:8}\033[1;0m".format(logging.getLevelName(logging.INFO)))
-    logging.addLevelName(logging.DEBUG, "\033[1;34m{:8}\033[1;0m".format(logging.getLevelName(logging.DEBUG)))
+    logging.addLevelName(logging.WARNING, "\033[1;31m{}\033[1;0m".format(logging.getLevelName(logging.WARNING)))
+    logging.addLevelName(logging.ERROR, "\033[1;35m{}\033[1;0m".format(logging.getLevelName(logging.ERROR)))
+    logging.addLevelName(logging.INFO, "\033[1;32m{}\033[1;0m".format(logging.getLevelName(logging.INFO)))
+    logging.addLevelName(logging.DEBUG, "\033[1;34m{}\033[1;0m".format(logging.getLevelName(logging.DEBUG)))
     # fmt: on
