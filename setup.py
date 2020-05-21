4a691e3dc8f92751e43a0efdf093aae8486749bb
Doug Davis <ddavis@ddavis.io>
2020-05-21 14:50:34 -0400

    fixing dist

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup
 
-setup(entry_points={"console_scripts": ["tdub = tdub.__main__:run_cli"]})
+setup(
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    entry_points={"console_scripts": ["tdub = tdub.__main__:run_cli"]}
+)
