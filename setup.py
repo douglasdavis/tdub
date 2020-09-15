63f54d565756332a534d6b6ed274dff8ab2917c3
Doug Davis <ddavis@ddavis.io>
2020-09-15 16:56:17 -0400

    add package_data for wheel

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     entry_points={"console_scripts": ["tdub = tdub.__main__:run_cli"]},
+    package_data={"": ["*.typed"]},
 )
