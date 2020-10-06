415ec003fe110562b5ceb5ffe8f16de07f9aacb9
Doug Davis <ddavis@ddavis.io>
2020-10-06 10:42:22 -0400

    move entry point to setup.cfg

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import setup
 setup(
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    entry_points={"console_scripts": ["tdub = tdub.__main__:run_cli"]},
     package_data={"": ["*.typed"]},
 )
